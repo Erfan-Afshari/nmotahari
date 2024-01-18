8a39ae151c185199ddc92826f0676debf022ab06
Erfan-Afshari <erfanafshari8787@gmail.com>
2024-01-18 19:33:11 +0330

    add api

--- app/api/news/route.js
+++ app/api/news/route.js
@@ -3,7 +3,7 @@ import news from "./server.json"
 
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://example.com'
+export const server = dev ? 'http://localhost:3000' : 'https://nmotahari.netlify.app/'
 
 export async function GET(request) {
     return NextResponse.json(news)
