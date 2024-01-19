228fe4ea1621b40431ba68df2ca9092b31d3b58b
Erfan-Afshari <erfanafshari8787@gmail.com>
2024-01-19 22:05:55 +0330

    update

--- app/components/Dashboard.jsx
+++ app/components/Dashboard.jsx
@@ -13,11 +13,11 @@ const Dashboard = () => {
 
   return (
 	<div>
-		{cookie["user-profile"] ?
+		{/* {cookie["user-profile"] ? */}
 			<div className="h-[30vh]">
-			  {/* <p className={`${Vazir.className} ${"text-center mt-12"}`}>خوش آمدی {cookie["user-profile"].name}</p> */}
+			  <p className={`${Vazir.className} ${"text-center mt-12"}`}>خوش آمدی {cookie["user-profile"].name}</p>
 			</div> 
-		: router.push('/signin')}
+		{/* : router.push('/signin')} */}
 	</div>
   )
 }
