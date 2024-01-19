b98fef7a5eb6280e8b10795f717b9b9e324cfaa3
Erfan-Afshari <erfanafshari8787@gmail.com>
2024-01-19 21:30:31 +0330

    update

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,17 +1,34 @@
-'use client'
-import { useRouter } from 'next/navigation';
-import Dashboard from '@/app/components/Dashboard';
+// import Dashboard from '@/app/components/Dashboard';
 
-const page = () => {
+// const page = () => {
+//   return (
+// 	<>
+// 		{/* <Dashboard />  */}
+// 	</>
+//   )
+// }
+
+// export default page
+
+
+
+'use client'
+import { useRouter } from "next/navigation"
+import { useCookies } from "react-cookie"
+import { Vazir } from "../utils/fonts";
+
+const Dashboard = () => {
 	const router = useRouter();
-	if (router.isFallback) {
-		<h1>Data is loading</h1>;
-	}
+	const [cookie, setCookie] = useCookies(["user-profile"])
   return (
 	<div>
-		<Dashboard /> 
+		{cookie["user-profile"] ?
+			<div className="h-[30vh]">
+			  <p className={`${Vazir.className} ${"text-center mt-12"}`}>خوش آمدی {cookie["user-profile"].name}</p>
+			</div> 
+		: router.push('/signin')}
 	</div>
   )
 }
 
-export default page
+export default Dashboard
